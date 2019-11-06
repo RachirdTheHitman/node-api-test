225289b53644f17d77cf8093b35fc81c7a7dad06
Richard <hyacinthzy1@gmail.com>
2019-11-06 15:12:26 +1100

    deleted some unuseful files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 require("./db/mongoose");
 const User = require("./models/user");
 const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
 const cors = require('cors')
 
 const app = express();
@@ -11,7 +10,6 @@ const port = process.env.PORT;
 app.use(cors())
 app.use(express.json());
 app.use(userRouter)
-app.use(taskRouter)
 
 
 app.listen(port, () => {
