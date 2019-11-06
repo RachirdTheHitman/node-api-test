ef4be0d58b1fe340d8d4c33e9ac3e75989adcc54
Richard <hyacinthzy1@gmail.com>
2019-11-06 18:00:23 +1100

    adding jest testing for User router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-const express = require("express");
-require("./db/mongoose");
-const userRouter = require('./routers/user')
-const cors = require('cors')
+const app = requrie('./app.js')
 
-const app = express();
 const port = process.env.PORT;
-
-app.use(cors())
-app.use(express.json());
-app.use(userRouter)
-
-
 
 app.listen(port, () => {
   console.log("Server is up on port: " + port);
---
+++ tests/user.test.js
@@ -0,0 +1,59 @@
+const request = require("supertest");
+const app = require("../src/app");
+const User = require("../src/models/user");
+
+const userOne = {
+  firstname: "richard",
+  lastname: "zhang",
+  paydate: "06 November 2019",
+  payfreq: "monthly",
+  annualsalary: 70000,
+  grossIncome: 5833,
+  incomeTax: 1192,
+  netIncome: 4641,
+  super: 524,
+  pay: 4117
+};
+
+//delete all the user documents in Mongodb to setup the testing environment
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+});
+
+//create a new payslip for a payee
+test("Should create a new user", async () => {
+  await request(app)
+    .post("/users")
+    .send({
+      firstname: "john",
+      lastname: "smith",
+      paydate: "06 November 2019",
+      payfreq: "monthly",
+      annualsalary: 80000,
+      grossIncome: 6666,
+      incomeTax: 1463,
+      netIncome: 5203,
+      super: 599,
+      pay: 4604
+    })
+    .expect(201);
+});
+
+// test when trying to pay a same person twice
+test("Should fail the test and receive 400 bad quest when trying creating a same user, meaning who's been paid already", async () => {
+  await request(app)
+    .post("/users")
+    .send({
+      firstname: "richard",
+      lastname: "zhang",
+      paydate: "06 November 2019",
+      payfreq: "monthly",
+      annualsalary: 70000,
+      grossIncome: 5833,
+      incomeTax: 1192,
+      netIncome: 4641,
+      super: 524,
+      pay: 4117
+    }).expect(400);
+});
