489232e0a6cc0eee44c2152799af25a87e83d3e8
Richard <hyacinthzy1@gmail.com>
2019-11-06 14:45:11 +1100

    installed cors

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ require("./db/mongoose");
 const User = require("./models/user");
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
+const cors = require('cors')
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(cors())
 app.use(express.json());
 app.use(userRouter)
 app.use(taskRouter)
--- src/models/user.js
+++ src/models/user.js
@@ -37,11 +37,86 @@ const userSchema = new mongoose.Schema({
         throw new Error("Age must be a positive number");
       }
     }
+  },
+  payDate: {
+    type: Date,
+    default: Date.now
+  },
+  payFrequency: {
+    type: String,
+    default: 'Monthly'
+  },
+  annualIncome: {
+    type: Number,
+    required: true,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("annualIncome must be a positive number");
+      }
+    }
+  },
+  grossIncome: {
+    type: Number,
+    required: true,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("grossIncome must be a positive number");
+      }
+    }
+  },
+  incomeTax: {
+    type: Number,
+    required: true,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("incomeTax must be a positive number");
+      }
+    }
+  },
+  netIncome: {
+    type: Number,
+    required: true,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("netIncome must be a positive number");
+      }
+    }
+  },
+  super: {
+    type: Number,
+    required: true,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("super must be a positive number");
+      }
+    }
+  },
+  pay: {
+    type: Number,
+    required: true,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("pay must be a positive number");
+      }
+    }
   }
 });
 
 userSchema.pre("save", async function(next) {
   const user = this;
+
+  const userRetrieved = await User.find({ name: user.name });
+  if (userRetrieved.length) {
+    console.log("userRetrieved", userRetrieved);
+    console.log("user exists: ", user.name);
+    next(new Error("The user exists and already be paid in this month!"));
+  }
 
   if (user.isModified("password")) {
     user.password = await bcrypt.hash(user.password, 8);
